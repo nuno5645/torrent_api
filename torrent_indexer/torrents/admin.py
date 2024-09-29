6249870deec6f81e84b47447ae7db603548ed0f2
Nuno <ntrjferreira@hotmail.com>
2024-09-29 14:22:25 +0100

    models to admin control

--- torrent_indexer/torrents/admin.py
+++ torrent_indexer/torrents/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Movie, WatchHistory, StreamingList, StreamingListMovie, Torrent, RealDebrid, TVShow, Season, Episode, TVShowWatchHistory, TVShowStreamingList, TVShowStreamingListShow
 
-# Register your models here.
+# Register your models here
+admin.site.register(Movie)
+admin.site.register(WatchHistory)
+admin.site.register(StreamingList)
+admin.site.register(StreamingListMovie)
+admin.site.register(Torrent)
+admin.site.register(RealDebrid)
+admin.site.register(TVShow)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(TVShowWatchHistory)
+admin.site.register(TVShowStreamingList)
+admin.site.register(TVShowStreamingListShow)
